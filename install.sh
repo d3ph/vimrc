864880845612b692d57d5232b134d64d84e54888
d3ph <d3ph.ru@gmail.com>
2017-01-20 16:45:46 +0300

    for sierra

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set +e
 
 sudo apt-get install vim-nox
-bashrc=~/.bashrc
+bashrc=~/.bash_profile
 inject_bashrc="source ~/vimrc/$(basename $bashrc)"
 
 test -f $bashrc && grep -q -F "$inject_bashrc" $bashrc || echo "$inject_bashrc" >> $bashrc
