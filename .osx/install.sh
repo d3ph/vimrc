b43477b9d062dd3fded0daee0aad80e5571aa61c
d3ph <d3ph.ru@gmail.com>
2017-05-23 14:19:23 +0300

    updatedb

--- .osx/install.sh
+++ .osx/install.sh
@@ -8,10 +8,11 @@ defaults write -g WebAutomaticTextReplacementEnabled -bool false
 defaults write com.apple.finder AppleShowAllFiles YES
 defaults write com.google.Keystone.Agent checkInterval 0
 # https://www.java.com/ru/download/mac_download.jsp
-# intellij
+# intellij - look for - Education keys for IDE(gmail)
 # https://docs.docker.com/docker-for-mac/
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew update
 brew upgrade
 brew bundle
+sudo /usr/libexec/locate.updatedb
