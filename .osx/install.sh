c8a0bf2d111525d7c1135224966601a9e1bde66a
d3ph <d3ph.ru@gmail.com>
2018-02-16 15:24:31 +0300

    more to preinstall osx

--- .osx/install.sh
+++ .osx/install.sh
@@ -7,6 +7,12 @@ defaults write -g WebAutomaticDashSubstitutionEnabled -bool false
 defaults write -g WebAutomaticTextReplacementEnabled -bool false
 defaults write com.apple.finder AppleShowAllFiles YES
 defaults write com.google.Keystone.Agent checkInterval 0
+defaults write com.apple.diskspaced minFreeSpace 4
+defaults write com.apple.diskspaced freeSpaceWarningLevel 1
+defaults write com.apple.diskspaced warningInterval 3600
+launchctl stop com.apple.diskspaced
+killall -STOP diskspaced
+launchctl start com.apple.diskspaced
 # https://www.java.com/ru/download/mac_download.jsp
 # intellij - look for - Education keys for IDE(gmail)
 # https://docs.docker.com/docker-for-mac/
@@ -14,5 +20,9 @@ defaults write com.google.Keystone.Agent checkInterval 0
 brew doctor
 brew update
 brew upgrade
+brew cleanup
 brew bundle
+# brew deps pkg
+# brew uses --installed pkg
+# brew list | while read cask; do echo -n $cask; brew deps "$cask  ->" | awk '{printf(" %s ", $0)}'; echo ""; done
 sudo /usr/libexec/locate.updatedb
